d337af47c17a182a0243e7fa9b653d81fe6e18e3
Ionel Gonta <ionel.gonta@gmail.com>
2025-12-27 17:37:44 +0200

    Fix Next.js image optimization for production domain

--- next.config.js
+++ next.config.js
@@ -19,6 +19,11 @@ const nextConfig = {
         port: '3000',
         pathname: '/uploads/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'victoriaocara.com',
+        pathname: '/uploads/**',
+      },
     ],
   },
   typescript: {
